423fd0ed4d1e89f1a1a9fc718478c9c00fc2f8d4
ouchengguo <chengguo.ou@amlogic.com>
2022-06-16 22:31:30 +0800

    add epg

---
+++ the-flash-zbhk/current/config.js
@@ -0,0 +1,10 @@
+var global_config = {
+    projectCode: 'ydjd',
+    startPictureUrl: 'http://ottpic.bbtv.cn/live_launcher/ydjd/ydjd_start_picture.jpg',
+    isPerformanceScoreOpen: false,
+    eplHost: "http://10.98.29.48:8080/BugPages/amlogic/EPG/APPService/index.php",
+    appLimitInitTime: "6000",
+    playTokenValidTime: 19 * 3600,
+    authResultCacheTime: 4 * 3600
+
+};
